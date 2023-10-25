8d6c2d0e6115a2d64ec499827fd2677524d2f2f8
Vidit <viditgoel2k@gmail.com>
2023-10-25 15:44:07 +0530

    addNewPost to the api using createAsyncThunk

--- src/components/AddPostForm.jsx
+++ src/components/AddPostForm.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { postAdded } from '../features/posts/postsSlice'
+import { addNewPost } from '../features/posts/postsSlice'
 import { selectAllUsers } from '../features/users/usersSlice'
 
 const AddPostForm = () => {
@@ -10,6 +10,7 @@ const AddPostForm = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const [userId, setUserId] = useState('')
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
     
     const onTitleChanged = (e) => {
         setTitle(e.target.value)
@@ -23,13 +24,22 @@ const AddPostForm = () => {
         setUserId(e.target.value)
     }
 
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
+
     const onSavePostClicked = () => {
-        if(title && content){
-            dispatch(
-                postAdded(title, content, userId)
-            )
-            setTitle('')
-            setContent('')
+        if(canSave){
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({title, body: content, userId})).unwrap()
+
+                setTitle('')
+                setContent('')
+                setUserId('')  
+            } catch (err) {
+                console.error('Failed to save the post', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
         }
     } 
 
@@ -38,8 +48,6 @@ const AddPostForm = () => {
             {user.name}
         </option>
     ))
-
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
     return (
         <section>
